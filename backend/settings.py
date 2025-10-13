18819753e120e50c79f0faeee2b85f4fcde389b0
terussar <erussard624@gmail.com>
2025-10-13 17:48:52 +0200

    add settings for bucket S3 amazon to create persistent storage

--- backend/settings.py
+++ backend/settings.py
@@ -62,6 +62,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +108,26 @@ DATABASES = {
         default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
+}
+
+# AWS configuration for static files
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+
+
+STORAGES = {
+    # pour media file
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    # pour css et js 
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
 }
 
 
