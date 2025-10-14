370d7608d764d6aaea40ce331552ce3ebd10520f
terussar <erussard624@gmail.com>
2025-10-14 17:55:58 +0200

    retour version ok - feature connexion s'inscrire a revoir

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Movie, Actor, Comment
+from .models import Movie, Actor, Comment, Casting
 
 admin.site.register(Movie)
 admin.site.register(Actor)
+admin.site.register(Casting)
 admin.site.register(Comment)
